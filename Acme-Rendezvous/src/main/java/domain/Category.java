e26528e46935743e21cee174e6ce65b570e7d79b
alfredocampos96 <alfredocampos4b@gmail.com>
2018-03-11 19:59:00 +0100

    category añadida

---
+++ Acme-Rendezvous/src/main/java/domain/Category.java
@@ -0,0 +1,83 @@
+package domain;
+
+import java.util.Collection;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Category extends DomainEntity {
+
+	
+	private String name;
+	private String description;
+	
+	
+	//Relationships
+	
+	private Category parent;
+	private Collection<Category> childrens;
+	private Collection<Service> services;
+	
+	
+	@NotBlank
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	@NotBlank
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Valid
+	@ManyToOne(optional = true)
+	@NotNull
+	public Category getParent() {
+		return parent;
+	}
+	public void setParent(Category parent) {
+		this.parent = parent;
+	}
+	
+	@Valid
+	@NotNull
+	@OneToMany(mappedBy = "parentCategory")
+	public Collection<Category> getChildrens() {
+		return childrens;
+	}
+	public void setChildrens(Collection<Category> childrens) {
+		this.childrens = childrens;
+	}
+	
+	@Valid
+	@ManyToMany
+	@NotNull
+	public Collection<Service> getServices() {
+		return services;
+	}
+	public void setServices(Collection<Service> services) {
+		this.services = services;
+	}
+}
+
+
+
+
+
