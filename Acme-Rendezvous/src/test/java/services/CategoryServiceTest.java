8d6fec4b7a379353118dbc4d03bee5e7750b88b5
Adru18 <36424996+Adru18@users.noreply.github.com>
2018-03-23 22:33:18 +0100

    categoryServiceTest funcionando

--- Acme-Rendezvous/src/test/java/services/CategoryServiceTest.java
+++ Acme-Rendezvous/src/test/java/services/CategoryServiceTest.java
@@ -54,6 +54,7 @@ public class CategoryServiceTest extends AbstractTest {
 	}
 
 	@Test
+	//normal
 	public void testSave() {
 		super.authenticate("admin");
 		final Category category = this.categoryService.create();
@@ -63,5 +64,39 @@ public class CategoryServiceTest extends AbstractTest {
 		Assert.isTrue(this.categoryService.findAll().contains(save));
 		super.authenticate(null);
 	}
+	@Test(expected = IllegalArgumentException.class)
+	//un actor distinto de admin intenta guardar un category
+	public void testSave2() {
+		super.authenticate("manager1");
+		final Category category = this.categoryService.create();
+		category.setName("sampleCategory");
+		category.setDescription("this is a sample category");
+		final Category save = this.categoryService.save(category);
+		Assert.isTrue(this.categoryService.findAll().contains(save));
+		super.authenticate(null);
+	}
+	@Test
+	//borrar una categoria que no tiene ninguna relacion
+	public void testDelete() {
+		super.authenticate("admin");
+		final Category category = this.categoryService.create();
+		category.setName("sampleCategory");
+		category.setDescription("this is a sample category");
+		final Category save = this.categoryService.save(category);
+		Assert.isTrue(this.categoryService.findAll().contains(save));
+		this.categoryService.delete(save);
+		Assert.isTrue(!this.categoryService.findAll().contains(save));
 
+		super.authenticate(null);
+	}
+	@Test
+	//borrar una categoria de la base de datos
+	public void testDelete2() {
+		super.authenticate("admin");
+		final Category category = this.categoryService.findOne(super.getEntityId("category2"));
+		this.categoryService.delete(category);
+		Assert.isTrue(!this.categoryService.findAll().contains(category));
+
+		super.authenticate(null);
+	}
 }
